d4675a96d90b95efa8152cb9ce07170cd0bd8b48
lrwx00t <lrwx00t@gmail.com>
2022-04-15 11:17:21 +1000

    grafana

--- configs/join.sh
+++ configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.0.10.10:6443 --token c6xqif.prrxbwrc2hpocjlu --discovery-token-ca-cert-hash sha256:3f621be054f365ee7810021ef5447c629347510cd483f897ec1000b41ae92ae9 
+kubeadm join 10.0.10.10:6443 --token 3d03no.55e2q7hx2y3cex5s --discovery-token-ca-cert-hash sha256:57e9dd516180e4af32653b58d110638c76f95b462faa0a0085e8bdee957b6ff2 
